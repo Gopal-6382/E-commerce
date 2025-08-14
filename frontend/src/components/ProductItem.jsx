13f6d788aa0e6c443ed9f8058b353be4c47bf444
Gopal-6382 <gopalkrishnanto@email.com>
2025-08-14 19:26:59 +0530

    product item image solved

--- frontend/src/components/ProductItem.jsx
+++ frontend/src/components/ProductItem.jsx
@@ -7,11 +7,12 @@ const ProductItem = ({ id, image, name, price }) => {
   return (
     <Link to={`/product/${id}`} className="text-gray-700 cursor-pointer">
       <div className="overflow-hidden">
-        <img
-          src={image[0]}
-          alt={name}
-          className="hover:scale-110 transition ease-in-out"
-        />
+      <img
+  src={image[0]}
+  alt={name}
+  className="hover:scale-110 w-full h-[175px] object-cover transition ease-in-out"
+/>
+
         <h3 className="text-sm pt-3 pb-1 font-semibold">{name}</h3>
         <p className="text-gray-600 text-sm font-medium">
           {currency}
